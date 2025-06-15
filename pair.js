08db518e3c9a9e53e7b5e7e25f3b0eed40b1c1e8
ali-lasharivj <queensapna26@gmail.com>
2025-06-15 10:12:49 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "STARK-ALI~" + string_session;
+                        let md = "ALI-MD=" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `> *CONNECTED SUCCESSFULLY ✅*
 ╭────「 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 」────◆
